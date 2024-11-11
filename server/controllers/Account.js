4d7a440b0a91c9fd3b94fe45b572d4d441c52afd
Jennifer Pichardo <jenniferpichardo15@gmail.com>
2024-11-10 19:36:57 -0500

    Commit

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -1,12 +1,18 @@
 const models = require('../models');
 
-const { Account } = models;
+const Account = models.Account;
 
-const loginPage = (req, res) => res.render('login');
+const loginPage = (req, res) => {
+  return res.render('login');
+};
 
-const signupPage = (req, res) => res.render('signup');
+const signupPage = (req, res) => {
+  return res.render('signup');
+};
 
-const logout = (req, res) => res.redirect('/');
+const logout = (req, res) => {
+  return res.redirect('/');
+}
 
 const login = (req, res) => {
   const username = `${req.body.username}`;
@@ -40,7 +46,7 @@ const signup = async (req, res) => {
 
   try {
     const hash = await Account.generateHash(pass);
-    const newAccount = new Account({ username, password: hash });
+    const newAccount = new Account({username, password: hash});
     await newAccount.save();
     return res.json({ redirect: '/maker' });
   } catch (err) {
